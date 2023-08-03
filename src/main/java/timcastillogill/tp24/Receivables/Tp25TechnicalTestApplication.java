55af89e4dd9f204f317a64df6c2c9e1684b9b859
timcastillogill <timgillviola@gmail.com>
2023-08-03 11:23:16 +0100

    Further setup of project

---
+++ src/main/java/timcastillogill/tp24/Receivables/Tp25TechnicalTestApplication.java
@@ -0,0 +1,14 @@
+package timcastillogill.tp24.Receivables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp25TechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp25TechnicalTestApplication.class, args);
+		System.out.println("Server started...");
+	}
+
+}
---
+++ src/test/java/timcastillogill/tp24/Receivables/Tp25TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package timcastillogill.tp24.Receivables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp25TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
